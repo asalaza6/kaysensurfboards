10ebd8824e947e9575ee86ab2281a7dc056c0e8a
asalaza6 <55558620+asalaza6@users.noreply.github.com>
2020-04-22 19:34:22 -0700

    added under construction message

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ import Slideshow from "../components/Slideshow";
 export default function Index() {
   
   return (
+    <div>
       <Slideshow/>
+
+      <div className = "message">Website currently under construction</div>
+      <style jsx>{`
+      .message {
+        
+        font-size:24pt;
+      }`}</style>
+    </div>
   );
 }
