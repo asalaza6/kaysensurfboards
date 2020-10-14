e14e8521214b631d7c71b818970f89cf420f2f29
Alex Salazar <55558620+asalaza6@users.noreply.github.com>
2020-10-13 22:59:58 -0700

    Update contact.js

--- web_client/pages/contact.js
+++ web_client/pages/contact.js
@@ -9,34 +9,33 @@ export default class Contact extends Component {
       email: "",
       message: "",
     }
-    this.onSubmit = this.onSubmit.bind(this);
     this.sendEmail = this.sendEmail.bind(this);
     this.onChange = this.onChange.bind(this);
   }
-  async onSubmit(){
-    console.log(this.state);
-    let body = {
-      email: this.state.email,
-      name: this.state.name,
-      message: this.state.message
-    }
-    try{
-      const response = await fetch(`${configs.api.url}:${configs.api.port}/dashboard/contact`,{
-          method: "POST",
-          headers:{
-              token: localStorage.token,
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body)
-      });
-      const parseRes = await response.json();
-      console.log(parseRes);
-      alert('image successfully uploaded');
-      window.location = "/";
-  }catch(err){
-      console.log(err.message);
-  }
-  }
+  // async onSubmit(){
+  //   console.log(this.state);
+  //   let body = {
+  //     email: this.state.email,
+  //     name: this.state.name,
+  //     message: this.state.message
+  //   }
+  //   try{
+  //     const response = await fetch(`${configs.api.url}:${configs.api.port}/dashboard/contact`,{
+  //         method: "POST",
+  //         headers:{
+  //             token: localStorage.token,
+  //             "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(body)
+  //     });
+  //     const parseRes = await response.json();
+  //     console.log(parseRes);
+  //     alert('image successfully uploaded');
+  //     window.location = "/";
+  // }catch(err){
+  //     console.log(err.message);
+  // }
+  // }
   onChange(evt,type){
     if(type == "name"){
       this.setState({"name":evt.target.value});
@@ -66,23 +65,20 @@ export default class Contact extends Component {
     return (
       <div className = "container">
         <p className = "message">Fill the form below and I will contact you as soon as possible.</p>
-        <form  name="contact" onSubmit = {evt =>{return this.sendEmail(evt)}}>
+        <form className = "form" name="contact" onSubmit = {evt =>{return this.sendEmail(evt)}}>
             <input type="hidden" name="form-name" value="contact" />
-            <p>
-              <label>Your Name: <input type="text" name="user_name" onInput = {(evt)=>this.onChange(evt,"name")} type="text" name="name"/></label>
-            </p>
-            <p>
-              <label>Your Email: <input type="email" name="user_email" onInput = {(evt)=>this.onChange(evt,"email")} type="email" name="email"/></label>
-            </p>
-            <p>
-              <label>Message: <textarea type="message" name="message" onInput = {(evt)=>this.onChange(evt,"message")} name="message"></textarea></label>
-            </p>
-            <p>
-              <input type="submit" value="emailjs"/>
-              <button type="button" onClick = {this.onSubmit}>Send</button>
-            </p>
+            <label>Your Name: <input type="text" name="user_name" onInput = {(evt)=>this.onChange(evt,"name")} type="text" name="name"/></label>
+            <label>Your Email: <input type="email" name="user_email" onInput = {(evt)=>this.onChange(evt,"email")} type="email" name="email"/></label>
+            <label>Message: <textarea type="message" name="message" onInput = {(evt)=>this.onChange(evt,"message")} name="message"></textarea></label>
+            <input type="submit" value="Send"/>
           </form>
         <style jsx>{`
+          .form {
+            justify-content: center;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
+          }
           @font-face {
             font-family: 'BalooThambi';
             src:url('/fonts/BalooThambi2-Regular.ttf');
